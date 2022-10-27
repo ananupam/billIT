2152374e536ec295b02f92b3524501320e8986bd
Anjali Anupam <2019024@iiitdmj.ac.in>
2022-10-27 21:26:23 +0530

    establishing routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
 
 import './App.css';
+import Home from './components/home/Home';
+import DisplayAll from './components/displayAll/DisplayAll';
+import AddNew from './components/addNew/AddNew';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+      <Routes>
+          <Route exact path='/' element={<Home/>} />
+        </Routes>
+        <Routes>
+          <Route path='/dispalyall' element={<DisplayAll/>} />
+        </Routes>
+        <Routes>
+          <Route path='/addnew' element={<AddNew/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
